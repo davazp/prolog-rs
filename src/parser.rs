aadf520f8f88de1c5a939dbf796a2c15128a4ec9
David Vázquez Púa <davazp@gmail.com>
2021-12-14 23:10:09 +0100

    Clean dead_code warnings

--- src/parser.rs
+++ src/parser.rs
@@ -1,32 +1,40 @@
 lalrpop_mod!(pub grammar);
 
-mod tests {
-    use super::grammar;
+use crate::terms::Term;
 
+#[allow(dead_code)]
+fn parse_expr(str: &str) -> Result<Term, ()> {
+    let parser = grammar::ExprParser::new();
+    parser.parse(&str).map_err(|_| ())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_expr;
     #[test]
     fn grammar() {
-        assert!(grammar::ExprParser::new().parse("22").is_ok());
-        assert!(grammar::ExprParser::new().parse("true").is_ok());
-        assert!(grammar::ExprParser::new().parse("false").is_ok());
+        assert!(parse_expr("22").is_ok());
+        assert!(parse_expr("true").is_ok());
+        assert!(parse_expr("false").is_ok());
 
-        assert!(grammar::ExprParser::new().parse("plus(1,2,3)").is_ok());
-        assert!(grammar::ExprParser::new().parse("plus(1,2,3,)").is_ok());
-        assert!(grammar::ExprParser::new().parse("plus(1,2").is_err());
+        assert!(parse_expr("plus(1,2,3)").is_ok());
+        assert!(parse_expr("plus(1,2,3,)").is_ok());
+        assert!(parse_expr("plus(1,2").is_err());
 
-        assert!(grammar::ExprParser::new().parse("X(1,2)").is_err());
+        assert!(parse_expr("X(1,2)").is_err());
 
-        assert!(grammar::ExprParser::new().parse("X").is_ok());
-        assert!(grammar::ExprParser::new().parse("f(X)").is_ok());
+        assert!(parse_expr("X").is_ok());
+        assert!(parse_expr("f(X)").is_ok());
 
-        assert!(grammar::ExprParser::new().parse("2+X").is_ok());
-        assert!(grammar::ExprParser::new().parse("2+X*2").is_ok());
+        assert!(parse_expr("2+X").is_ok());
+        assert!(parse_expr("2+X*2").is_ok());
 
-        assert!(grammar::ExprParser::new().parse("(2+X)*2").is_ok());
+        assert!(parse_expr("(2+X)*2").is_ok());
 
-        assert!(grammar::ExprParser::new().parse("g(f(2+X))").is_ok());
+        assert!(parse_expr("g(f(2+X))").is_ok());
 
-        assert!(grammar::ExprParser::new().parse("test :- a").is_ok());
+        assert!(parse_expr("test :- a").is_ok());
 
-        assert!(grammar::ExprParser::new().parse("f((a,b))").is_ok());
+        assert!(parse_expr("f((a,b))").is_ok());
     }
 }
