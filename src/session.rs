6fa10e8fb345f0994694c49af737f0ee8011b143
David Vázquez Púa <davazp@gmail.com>
2021-12-21 19:57:25 +0100

    Prevent excessive copying of the resolvent

--- src/session.rs
+++ src/session.rs
@@ -4,6 +4,40 @@ use crate::terms::{Clause, Functor, Goals};
 use crate::unify::unify_functors_in_env;
 use std::collections::VecDeque;
 use std::io;
+use std::rc::Rc;
+
+#[derive(Clone)]
+struct Resolvent(Rc<ResolventNode>);
+enum ResolventNode {
+    Empty,
+    Item { head: Functor, rest: Resolvent },
+}
+
+impl Resolvent {
+    fn append(&self, goals: Vec<Functor>) -> Resolvent {
+        let mut resolvent = self.clone();
+        for g in goals.into_iter().rev() {
+            let node = ResolventNode::Item {
+                head: g,
+                rest: resolvent,
+            };
+            resolvent = Resolvent(Rc::new(node))
+        }
+        resolvent
+    }
+
+    fn empty() -> Resolvent {
+        Resolvent(Rc::new(ResolventNode::Empty))
+    }
+
+    fn from_goal(g: Functor) -> Resolvent {
+        let node = ResolventNode::Item {
+            head: g,
+            rest: Resolvent::empty(),
+        };
+        Resolvent(Rc::new(node))
+    }
+}
 
 pub struct Session {
     db: Database,
@@ -19,9 +53,9 @@ impl Session {
         })
     }
 
-    fn solve(&self, resolvent: &mut Goals, env: &mut Env, chr: u32) -> bool {
-        match resolvent.0.pop_front() {
-            None => {
+    fn solve(&self, resolvent: &Resolvent, env: &mut Env, chr: u32) -> bool {
+        match &*resolvent.0 {
+            ResolventNode::Empty => {
                 println!("solution:");
                 env.print();
                 println!("-------------------------");
@@ -37,24 +71,25 @@ impl Session {
                     true
                 }
             }
-            Some(selection) => {
-                let mut clauses = VecDeque::from(
-                    self.db
-                        .matching_clauses(&selection.name, selection.args.len()),
-                );
+            ResolventNode::Item {
+                head: first,
+                rest: remaining,
+            } => {
+                let mut clauses =
+                    VecDeque::from(self.db.matching_clauses(&first.name, first.args.len()));
                 for c in clauses.iter_mut() {
                     c.rename(chr)
                 }
-                self.prove(selection, clauses, resolvent, env, chr)
+                self.prove(&first, clauses, &remaining, env, chr)
             }
         }
     }
 
     fn prove(
         &self,
-        goal: Functor,
+        goal: &Functor,
         mut clauses: VecDeque<Clause>,
-        resolvent: &mut Goals,
+        resolvent: &Resolvent,
         env: &mut Env,
         chr: u32,
     ) -> bool {
@@ -66,17 +101,8 @@ impl Session {
             let clause = clauses.pop_front().unwrap();
 
             if unify_functors_in_env(env, &goal, &clause.head) {
-                let mut newresolvent: Goals = Goals(VecDeque::with_capacity(
-                    clause.body.0.len() + resolvent.0.len(),
-                ));
-
-                for lit in clause.body.0.iter() {
-                    newresolvent.0.push_back(lit.clone());
-                }
-                for lit in resolvent.0.iter() {
-                    newresolvent.0.push_back(lit.clone())
-                }
-                if self.solve(&mut newresolvent, env, chr + 1) {
+                let newresolvent = resolvent.append(Vec::from(clause.body.0));
+                if self.solve(&newresolvent, env, chr + 1) {
                     true
                 } else {
                     env.pop_frame();
@@ -89,9 +115,10 @@ impl Session {
         }
     }
 
-    pub fn query(&self, mut query: Goals) -> bool {
+    pub fn query(&self, query: Goals) -> bool {
         let mut env = Env::new();
-        let result = self.solve(&mut query, &mut env, 1);
+        let resolvent = Resolvent::append(&Resolvent::empty(), Vec::from(query.0));
+        let result = self.solve(&resolvent, &mut env, 1);
         if !result {
             println!("false");
         }
